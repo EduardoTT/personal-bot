e879e8e5a666164f2ddb7d8a1834978a424befd2
Eduardo Tolmasquim <eduardo@purplemetrics.com.br>
2026-03-22 22:37:27 -0300

    Create chat interface

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+def chat(request):
+    return render(request, "bot/chat.html")
+
+
+@require_http_methods(["POST"])
+def message(request):
+    user_message = request.POST.get("message", "").strip()
+    bot_response = user_message
+    return render(request, "bot/partials/message.html", {
+        "user_message": user_message,
+        "bot_response": bot_response,
+    })
